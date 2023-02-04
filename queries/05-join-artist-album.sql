2e1b6a805f7ff202441b1391fdec615a958d298d
daniellanorris <daniellanorris@ufl.edu>
2023-02-03 23:18:35 -0500

    changes

---
+++ queries/05-join-artist-album.sql
@@ -0,0 +1,7 @@
+-- Join artists on albums, pulling artists.name as artist_name
+-- albums.name as album_name and albums.release_year
+-- sort by artists.name ascending
+SELECT artists.name AS artist_name, albums.name AS album_name, albums.release_year AS release_year
+FROM artists
+JOIN albums ON artists.id = albums.artist_id
+ORDER BY artists.name ASC;
